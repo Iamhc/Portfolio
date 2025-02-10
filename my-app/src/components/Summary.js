3e1033e0c29e4864e76f3a3216f2767e6429025a
Iamhc <himanshuchaudhary586@gmail.com>
2025-02-11 00:05:16 +0530

    first commit

--- my-app/src/components/Summary.js
+++ my-app/src/components/Summary.js
@@ -1,17 +1,16 @@
 let Summary = () => {
   return (
     <div id="summary">
-      I am a passionate Full-Stack Web Developer with a
-      strong foundation in frontend and backend technologies. With expertise in
-      React.js, Three.js, Tailwind CSS, and WebGL, I specialize in creating
-      interactive, visually appealing, and user-friendly websites. My experience
-      extends to full-stack development using MongoDB, Express.js, React, and
-      Node.js (MERN Stack) and cloud-based deployments on platforms like Vercel
-      and Render. Beyond development, I have a keen eye for graphic design,
-      having worked on branding and social media content creation. My work
-      reflects a blend of creativity and technical precision, ensuring both
-      aesthetics and functionality in every project.
-
+      I am a passionate Full-Stack Web Developer with a strong foundation in
+      frontend and backend technologies. With expertise in React.js, Three.js,
+      Tailwind CSS, and WebGL, I specialize in creating interactive, visually
+      appealing, and user-friendly websites. My experience extends to full-stack
+      development using MongoDB, Express.js, React, and Node.js (MERN Stack) and
+      cloud-based deployments on platforms like Vercel and Render. Beyond
+      development, I have a keen eye for graphic design, having worked on
+      branding and social media content creation. My work reflects a blend of
+      creativity and technical precision, ensuring both aesthetics and
+      functionality in every project.
     </div>
   );
 };
