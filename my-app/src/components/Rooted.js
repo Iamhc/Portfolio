8e334f4d20c0472e46dd4fed7b2a196f178fb9e4
Iamhc <himanshuchaudhary586@gmail.com>
2025-10-10 12:06:18 +0530

    Updated portfolio content

--- my-app/src/components/Rooted.js
+++ my-app/src/components/Rooted.js
@@ -63,7 +63,7 @@ const Rooted = () => {
 
       // Different thresholds
       const verticalThreshold = 220;
-      const horizontalThreshold = 85; // less coefficient (more sensitive)
+      const horizontalThreshold = 120; // less coefficient (more sensitive)
 
       if (ticking.current) return;
 
