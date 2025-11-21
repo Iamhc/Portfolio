773103de5329df9fc3474ac7c7841d1a361b2d73
Himanshu choudhary <86661174+Iamhc@users.noreply.github.com>
2025-11-21 14:10:30 +0530

    Update Projects.js

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -23,19 +23,20 @@ const Project = () => {
         "Conversational AI app with React frontend, Node.js backend, and SQLite for chat history persistence.",
       link: "https://dear-gules.vercel.app/",
     },
-    {
-      title: "Swiggy Clone",
-      image: SwiggyClone,
-      description:
-        "Responsive Swiggy-like app using real-time Swiggy API data. Built with React and Tailwind CSS on the frontend, and Node.js/Express backend for handling API requests and CORS.",
-      link: "https://github.com/Iamhc/Swiggy",
-    },
+    ,
     {
   title: "OS Performance Dashboard",
   image: os,   // make sure you imported this image at top
   description:
     "Real-time OS performance monitoring dashboard built using Next.js, Node.js/Express backend, and Chart.js visualizations. Tracks CPU, memory, network, and disk usage using system-level APIs.",
   link: "https://github.com/Iamhc/os-performance-frontend/tree/master/my-next-app",
+    },
+    {
+      title: "Swiggy Clone",
+      image: SwiggyClone,
+      description:
+        "Responsive Swiggy-like app using real-time Swiggy API data. Built with React and Tailwind CSS on the frontend, and Node.js/Express backend for handling API requests and CORS.",
+      link: "https://github.com/Iamhc/Swiggy",
     },
 
     {
